23d95f8f186789f677a003884143317b42bfb2c9
Jonas Krook <krook.jonas@gmail.com>
2018-02-11 12:09:09 +0100

    Adding more sensors to the list. Making sure that we don't crash on unknown tags.

--- read_data.py
+++ read_data.py
@@ -7,10 +7,33 @@ def read_data(filename):
     Returns:
     Dict with lists of readings, where each reading is a list
     """
-    data = {'GYR': [], 'ACC': [], 'RSSWIFI': [], 'GPS': []}
-    for line in open(filename):
-        words = line.split("\t")
-        words[-1] = words[-1].strip()
-        tag = words[1]
-        data[tag].append([words[0]] + words[2:])
-        return data
+
+    data = {
+        'GYR': [],
+        'ACC': [],
+        'RSSWIFI': [],
+        'GPS': [],
+        'RAWGYR': [],
+        'PRX': [],
+        'MAG': [],
+        'RAWMAG': [],
+        'LGT': [],
+        'PRS': [],
+        'ORI': [],
+        'RSSBLE': [],
+        'RSSCELL': []}
+
+    # Keep track of the elements that weren't parsed.
+    unknown_tags = set()
+
+    with open(filename) as datafile:
+        for line in datafile:
+            words = line.split("\t")
+            words[-1] = words[-1].strip()
+            tag = words[1]
+            if tag in data:
+                data[tag].append([words[0]] + words[2:])
+            else:
+                # The tag is currently unknown.
+                unknown_tags.add(tag)
+    return data, unknown_tags
